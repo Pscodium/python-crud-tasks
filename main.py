e860aba550f574adaa2b57b99da35a615d433a60
Pscodium <peter-larson@hotmail.com>
2022-02-07 15:35:39 -0300

    feat(#2): frame de criaçao de tarefas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import *
-from turtle import update
+from tkinter import ttk
+
 
 white = '#ffffff'
 black = '#000000'
@@ -18,6 +19,54 @@ window.geometry('600x300')
 window.title('Tarefas')
 window.resizable(False, False)
 window.configure(bg=white)
+
+def create():
+
+    days = []
+    months = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez']
+    year = []
+
+    for i in range(1, 32):
+        days.append(i)
+
+    for i in range(1990, 2026):
+        year.append(i)
+
+
+    ## FRAME DE CRIAÇÃO DE TAREFA
+    create_frame = Frame(window, width=300, height=220, bg=color1)
+    create_frame.place(x=10, y=60)
+
+    ## LABEL DE TAREFA COM ENTRY
+    task_label = Label(create_frame, text='Tarefa', bg=color1, fg=black)
+    task_label.place(x=10, y=10)
+
+    task_entry = Entry(create_frame, text='Tarefa', bg=white, fg=black)
+    task_entry.place(x=10, y=30)
+
+    ## LABEL DE DATA COM COMBOBOX
+    date_label = Label(create_frame, text='Data', bg=color1, fg=black)
+    date_label.place(x=10, y=60)
+
+    day_combo = ttk.Combobox(create_frame, values=days, width=3)
+    day_combo.place(x=10, y=80)
+
+    month_combo = ttk.Combobox(create_frame, values=months, width=5)
+    month_combo.place(x=55, y=80)
+
+    year_combo = ttk.Combobox(create_frame, values=year, width=5)
+    year_combo.place(x=117, y=80)
+
+    description_label = Label(create_frame, text='Descrição', bg=color1, fg=black)
+    description_label.place(x=10, y=110)
+
+    description = Text(create_frame, bg=white, fg=black, width=34, height=4)
+    description.place(x=10, y=130)
+
+
+
+
+
 
 
 create = Button(window, text='Novo', activebackground=color1, command=create, bg=color1, fg=entry, cursor='hand2', border=0, highlightbackground=color1, activeforeground=black)
